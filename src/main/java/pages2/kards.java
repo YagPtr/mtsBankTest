3de88695fabad8cd6189349e995e72edbe8ca7a5
Yag <criper294@gmail.com>
2025-01-05 15:38:11 +0300

    тест селениума 4

--- src/main/java/pages2/kards.java
+++ src/main/java/pages2/kards.java
@@ -1,4 +1,16 @@
 package pages2;
 
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.Keys;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.sleep;
 public class kards {
+    private final SelenideElement all= $x("//div[@class=\"TabsBarInner-sc-a22f654e-7 kMifUc\"]//a//div[contains(text(),\"Все\")]");
+
 }
